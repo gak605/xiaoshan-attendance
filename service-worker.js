6cb1afda18862d922327fbdfef89391b4dadfa7f
gak605 <kvnrrb25s6@privaterelay.appleid.com>
2026-07-07 21:56:56 +0800

    v1.0-dev-011 Attendance Action Sheet

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='xiaoshan-v100-dev010-button-time-hardfix';
-const ASSETS=['./?v=100dev010','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE_NAME='xiaoshan-v100-dev011-attendance-action-sheet';
+const ASSETS=['./?v=100dev011','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))) });
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(c=>c||caches.match('./index.html'))))});
