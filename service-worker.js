e16a473c15918224bbd0165242eae80956bdc357
gak605 <kvnrrb25s6@privaterelay.appleid.com>
2026-07-08 15:36:47 +0800

    v0.5.12 PWA Cache Version Fix

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='xiaoshan-v100-dev013-core-replace';
-const ASSETS=['./?v=100dev013','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE_NAME='xiaoshan-v100-dev014-core-replace';
+const ASSETS=['./?v=100dev014','./index.html','./manifest.webmanifest?v=100dev014','./icon-192.png?v=100dev014','./icon-512.png?v=100dev014'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))) });
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(c=>c||caches.match('./index.html'))))});
